b9b9ac63643daa36febe7da76d99aa1f9510b5df
ranelho <ranelhol@gmail.com>
2024-04-04 09:28:42 -0300

    first commit

---
+++ src/main/java/com/rlti/apicontracheque/ApiDeEmissaoDeContrachequeApplication.java
@@ -0,0 +1,15 @@
+package com.rlti.apicontracheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ApiDeEmissaoDeContrachequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeEmissaoDeContrachequeApplication.class, args);
+	}
+
+}
