0c118ef7a1ca7556e487e201a47b63b3189d510e
ranelho <ranelhol@gmail.com>
2024-04-09 20:34:46 -0300

    ajuste retorno api

--- src/main/java/com/rlti/apicontracheque/service/ContrachequeApplicationService.java
+++ src/main/java/com/rlti/apicontracheque/service/ContrachequeApplicationService.java
@@ -2,11 +2,15 @@ package com.rlti.apicontracheque.service;
 
 import com.rlti.apicontracheque.request.ContrachequeRequest;
 import com.rlti.apicontracheque.response.ContrachequeResponse;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ContentDisposition;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import org.springframework.http.HttpHeaders;
 
 @Service
 @RequiredArgsConstructor
@@ -17,18 +21,28 @@ public class ContrachequeApplicationService implements ContrachequeService {
 
     @Override
     public String gerarContracheque(ContrachequeRequest contrachequeRequest) {
-        ContrachequeResponse contrachequeResponse = rhClient.getContracheque(contrachequeRequest);
-        return JasperReports.gerarContrachequeBase64(contrachequeResponse);
+        try {
+            ContrachequeResponse contrachequeResponse = rhClient.getContracheque(contrachequeRequest);
+            return JasperReports.gerarContrachequeBase64(contrachequeResponse);
+        } catch (FeignException e) {
+            return String.valueOf(new Exception(e.getMessage()));
+        }
     }
 
     @Override
     public ResponseEntity<byte[]> gerarContracheque2(ContrachequeRequest contrachequeRequest) {
-        ContrachequeResponse contrachequeResponse = rhClient.getContracheque(contrachequeRequest);
-        byte[] pdfBytes = JasperReports.gerarContrachequePdf(contrachequeResponse);
-        return ResponseEntity
-                .ok()
-                .contentType(MediaType.APPLICATION_PDF)
-                .body(pdfBytes);
+        try {
+            ContrachequeResponse contrachequeResponse = rhClient.getContracheque(contrachequeRequest);
+            byte[] pdfBytes = JasperReports.gerarContrachequePdf(contrachequeResponse);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDisposition(ContentDisposition.builder("attachment").filename("contracheque.pdf").build());
+
+            return ResponseEntity.ok().headers(headers).body(pdfBytes);
+        } catch (FeignException e) {
+            return ResponseEntity.status(e.status()).body(e.getMessage().getBytes());
+        }
     }
 
 
