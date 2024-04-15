3e5a4b286afd405e51dc854c47b049dfee36f9d1
ranelho <ranelhol@gmail.com>
2024-04-15 14:14:02 -0300

    validação request

--- src/main/java/com/rlti/apicontracheque/request/ContrachequeRequest.java
+++ src/main/java/com/rlti/apicontracheque/request/ContrachequeRequest.java
@@ -1,7 +1,13 @@
 package com.rlti.apicontracheque.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record ContrachequeRequest(
+        @NotBlank(message = "Mes competência é obrigatório!")
+        @Pattern(regexp = "^(?:\\d{4})-(?:0[1-9]|1[0-2])$", message = "Formato de ano-mês inválido")
         String mesAno,
+        @NotBlank(message = "Número de matricula é obrigatório!")
         String matricula
 ) {
 }
